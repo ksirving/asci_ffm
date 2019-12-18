fdc1d2563e449ad1a39f34a9da067c1a30eb65d4
ksirving <32795054+ksirving@users.noreply.github.com>
2019-12-18 14:03:33 -0800

    updated code
    cleaning algae code
    spatial joins - first commit

--- code/upload_asci_sites.R
+++ code/upload_asci_sites.R
@@ -11,19 +11,15 @@ library(raster)
 
 #  statewide study - asci - ffm
 
-# bugs <- read.csv("/Users/katieirving/Documents/Projects/UC Davis/data/Taxonomy_Ryan.csv")
-# head(bugs)
 #  upload algae data - asci scores downloaded from https://sites.google.com/view/asci/results - Susie
 
 setwd("/Users/katieirving/Documents/git/asci_ffm-master")
 
 asci_scor <- read.csv("input_data/asci.scores_dec2019.csv", header=T)
 head(asci_scor)
-#  use MMI (d=diatom, sha=soft algae, hybrid = both)bugs <- read_csv(file = "/Users/katieirving/Documents/Projects/UC Davis/data/Taxonomy_Ryan.csv") %>% 
+#  use MMI (d=diatom, sha=soft algae, hybrid = both)
 
-
-# subset to only sites and hybrid MMI score
-# asci_scor <- asci_scor[,c(1,7)]
+# change sample id name
 colnames(asci_scor)[1] <- "SampleID"
 
 #  upload site details - for coordinates
@@ -51,109 +47,123 @@ dim(asci_scor_sites) # 128764      4
 str(algae_sites)
 #  lose ~2K Samples
 
+#  remove duplicates
 asci_scor_sites <- asci_scor_sites[!duplicated(asci_scor_sites),]
 dim(asci_scor_sites) #2625    4 - more sites here than the asci scores df as some samples have 2x reps
 #  remmove NAs
-sum(is.na(asci_scor_sites))
+sum(is.na(asci_scor_sites)) # some NAs from missing data in 0overE - keep MMIs from these sites? progress without but can change
 asci_scor_sites <- na.omit(asci_scor_sites)
 
-
-#  remove duplicates
-
 head(asci_scor_sites)
-write.csv(asci_scor_sites, "/Users/katieirving/Documents/git/asci_ffm-master/output_data/asci_scores_coords.csv")
+write.csv(asci_scor_sites, "output_data/asci_scores_coords.csv")
+str(asci_scor_sites)
+# change from factor to character
+asci_scor_sites$SampleID_old <- as.character(asci_scor_sites$SampleID_old)
 
 #  split date - station IDs etc
 
-?separate
 # use lubridate/tidyr to fix dates to a consistent format
-algae <- separate(asci_scor_sites, col = SampleID_old , into=c("AA", "BB", "YYYY"), remove = T) %>% 
-  mutate(AA=as.integer(AA),
-         BB=as.integer(BB)) %>% 
-  mutate(
-    "MM"=case_when(
-      AA > 12 ~ BB,
-      AA < 13 ~ AA),
-    "DD" = case_when(
-      AA > 12 ~ AA,
-      AA < 13 ~ BB),
-    "YYYY" = as.integer(YYYY),
-    "sampledate" = ymd(paste0(YYYY,"-", MM, "-",DD))
-  ) %>% 
-  dplyr::select(StationCode:YYYY, MM, DD, sampledate, everything(), -c(AA, BB))
+algae <- separate(asci_scor_sites, col = SampleID_old , into=c("StationID", "MM", "DD", "YY", "Rep", "Rep2"), remove = F) 
 
-# how many missing SampleID's?: 7991
-sum(is.na(bugs$SampleID))
+# SampleID: recommended format is "stationcode_sampledate_collectionmethodcode_fieldreplicate" - already set this way in algae
+
+
+# 1679, 1680, 1685, 1686, 1687, 1700, 2199, 2200, 2201, 2202, 2203, 2204, 2205, 2206, 2207, 2208, 2209, 2210, 2211, 2212, ...].
+## 26 have extra elements to split - need to be kept for StationID
+
+
+#  all station IDs with the extra element
+weird_station_IDs <- unique(algae[c(1679, 1680, 1685, 1686, 1687, 1700, 2199:2217, 2242), 2])
+
+# vector with index positions
+wsid <- algae$StationID %in% weird_station_IDs
+#  change values using index
+algae[wsid,2] <- paste(algae$StationID[wsid], "_", algae$MM[wsid], sep="")
+algae[wsid,3] <- paste(algae$DD[wsid])
+algae[wsid,4] <- paste(algae$YY[wsid])
+algae[wsid,5] <- paste(algae$Rep[wsid])
+algae[wsid,6] <- paste(algae$Rep2[wsid])
+
+ # add sample date
+algae$sampledate = ymd(paste0(algae$YY,"-", algae$MM, "-",algae$DD))
+
+#  remove Rep 2 column 
+
+algae$Rep2 <- NULL
+sum(is.na(algae)) # 0 
+
+#  look at oldest date - to match with FFM
+sort(unique(as.Date(algae$sampledate)))[1] # 2007-06-05
+
+dim(algae) ## 2260
+head(algae)
+
+# how many missing SampleID's?: 
+sum(is.na(algae$SampleID_old))
 
 # how many missing Sampledates?
-sum(is.na(bugs$sampledate))
+sum(is.na(algae$sampledate))
 
-coords <- asci_scor_sites[, 2:3]
-#  coords into numeric
-asci_scor_sites$Latitude <- as.numeric(as.character(asci_scor_sites$Latitude))
-asci_scor_sites$Longitude <- as.numeric(as.character(asci_scor_sites$Longitude))
+##############
+## some sites have reps 1 & 2 with different values- some sites are duplicated with
+# the same values
 
-#  remove NAs
-sum(is.na(asci_scor_sites)) #50
-asci_scor_sites <- na.omit(asci_scor_sites)
+# remove duplicated same values - differ only due to coords dp
 
-#  spatial point df
-# coordinates(asci_scor_sites) <- c("Latitude","Longitude")
-asci_scor_sites <- asci_scor_sites %>% 
-  st_as_sf(coords=c("Longitude", "Latitude"), crs=4326, remove=F) # define coords to make spatial
+#  remove duplicates
+algae <- algae[!duplicated(algae$SampleID_old),]
+dim(algae) # 2223
+head(algae)
 
-plot(asci_scor_sites)
-save(asci_scor_sites, file="output_data/asci_scores_coords.RData")
+##### below code regarding reps - average?????? to be finished
 
-
-# load(file="/Users/katieirving/Documents/git/bmi_ffm_links/data_output/bmi_trc_metrics_safit1.rda")
-# head(bmi_trc)
-# str(bmi_trc)
-
-#  component metrics for Algae? only OoverE & MMI for diatoms and soft bodied
-
-#  bug data
-
-load(file="/Users/katieirving/Documents/git/bmi_ffm_links/data_output/bmi_cleaned_all.rda")
-
-bmi_sites <- bmi_clean[,c(3,6:7)]
-# dim(bmi_sites) # 310216      3
-bmi_sites2 <- distinct(bmi_sites)
-write.csv(bmi_sites2, "/Users/katieirving/Documents/Projects/CEFF/GIS/bmi_sites.csv") 
-# dim(bmi_sites2) # 5627    3
-
-bmi_sites2 <- bmi_sites2 %>% 
-  st_as_sf(coords=c("longitude", "latitude"), crs=4326, remove=F) # define coords to make spatial
-
-
-#  compare algae sites with bmi sites
-plot(bmi_sites2)
-plot(asci_scor_sites)
-head(asci_scor_sites)
-head(bmi_sites2)
-write.csv(asci_scor_sites, "/Users/katieirving/Documents/Projects/CEFF/GIS/asci_sites.csv") 
-
-#  pair sites
-#  distance between sites
-
-install.packages("spatstat")
-# library(spatstat)
+#  create new station id column with id, date but no rep number
+# algae$SampleID_new <- paste(algae$StationID, "_", algae$MM, "/", algae$DD, "/", algae$YY, sep="")
 # 
-# install.packages("geosphere")
-# library(geosphere)
+# #  count number of repeated stationids
+# SampleID_freq <- as.data.frame(table(algae$SampleID_new))
+# #  extract stations with more than 1 rep 
+# str(SampleID_freq)
+# SampleID_freq$Freq <- as.numeric(as.character(SampleID_freq$Freq))
+# station_reps <- subset(stationID_freq, Freq > 1)
+# dim(station_reps)
+# station_reps
 # 
-# dists <- distHaversine(bmi_sites, asci_sites)
+# #  list stations with more than 1 rep
+# station_repsx <- station_reps$Var1
+# length(station_repsx) #187
+# 
+# #  subset asci data by stations with reps
+# algae_reps <- subset(algae, SampleID_new %in% station_repsx)
+# head(algae_reps)
+# dim(algae_reps) # 382
+# 
+# #  look at station with 3 x reps
+# ind <- algae_reps$SampleID_new %in% paste("405BH2Axx_06/16/10")
+# algae_reps[ind,]
 
-#  check same crs
-st_crs(bmi_sites2)
-st_crs(asci_scor_sites)
-
-?st_join
-?inner_join
-
-test_join <- st_join(bmi_sites2, asci_scor_sites, join=st_within, left=F)
-head(test_join)
+#  reps have different values - mean? 
+# i=1
+# 
+# ind <- algae_reps$SampleID_new %in% paste(station_repsx[i])
+# ind
+# 
+# algae_reps[ind,]
 
 
+#             SampleID_old StationID MM DD YY Rep    Latitude
+# 81  102PS0177_08/28/12_1 102PS0177 08 28 12   1 41.92939101
+# 107 102PS0177_08/28/12_2 102PS0177 08 28 12   2 41.92939101
+#       Longitude  OoverE.d    MMI.d OoverE.sba   MMI.sba
+# 81  -123.5725438 0.9580512 1.139708  1.0853745 1.0883831
+# 107 -123.5725438 0.7715050 1.126726  0.7384906 0.9756199
+# OoverE.hybrid MMI.hybrid sampledate       SampleID_new
+# 81      0.8719727   1.040264 2012-08-28 102PS0177_08/28/12
+# 107     0.7571862   1.083255 2012-08-28 102PS0177_08/28/12
 
+#  remiove unwated columns and save
+
+algae <- algae[, -c(3:6)]
+head(algae)
+save(algae, file="output_data/clean_algae.RData")
 
